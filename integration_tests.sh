84b849ac756edce1b92cbf74d5bf42408f361c3b
James Halsall <james.halsall@inviqa.com>
2021-09-02 11:33:15 +0100

    Add timeout on integration tests

--- integration_tests.sh
+++ integration_tests.sh
@@ -4,4 +4,4 @@ docker-compose down && docker-compose up -d
 echo "waiting for stack..."
 sleep 5
 
-LOG_LEVEL=error go test -count=1 -v --tags=integration ./integration/
+LOG_LEVEL=error go test -timeout=100s -count=1 -v --tags=integration ./integration/
