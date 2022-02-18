4a9a70948a9a102ec9eb3c4fc607c3f0dcddcecc
James Halsall <james.t.halsall@googlemail.com>
2022-02-18 11:39:26 +0000

    Wait longer between connection attempts to Kafka

--- vars.go
+++ vars.go
@@ -4,7 +4,7 @@ import "time"
 
 var (
 	maxConnectionAttempts      = 10
-	connectionInterval         = time.Millisecond * 500
+	connectionInterval         = time.Second * 1
 	dbRetryPollInterval        = time.Second * 5
 	defaultMaintenanceInterval = time.Hour * 1
 	defaultKafkaConnector      = connectToKafka
