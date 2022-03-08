470358a9f9d3948ba243fa73226d31ba50560739
James Halsall <james.t.halsall@googlemail.com>
2022-03-08 13:39:20 +0000

    Increase max connection attempts to 20

--- vars.go
+++ vars.go
@@ -3,7 +3,7 @@ package consumer
 import "time"
 
 var (
-	maxConnectionAttempts      = 10
+	maxConnectionAttempts      = 20
 	connectionInterval         = time.Second * 1
 	dbRetryPollInterval        = time.Second * 5
 	defaultMaintenanceInterval = time.Hour * 1
